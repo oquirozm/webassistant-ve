708446e8ed1fa14e1e4592bf5503e71ef829e41d
oquirozm <omarquirozmijares@gmail.com>
2017-05-04 03:02:43 -0700

    Online Application form ready to deploy.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,47 @@
 $(document).ready(function() {
-  console.log('Custom script is working. Alakazam.');
-})
+  
+    var requirementsContent = $('.requirements-container-1');
+    var requirementsToggleButton = $('.requirements .title');
+
+    var applicationForm = $('.form');
+    var buttonForm = $('.application-container-1 .button');
+
+    buttonForm.on('click', function() {
+        applicationForm.slideToggle(500);
+
+        if ( applicationForm.is(':visible') ) {
+            $('html, body').animate({
+                scrollTop : applicationForm.offset().top
+            });
+        }
+    });
+
+    requirementsToggleButton.on('click', function() {
+        requirementsContent.slideToggle();
+    });
+
+    // Validate form
+
+    $('.form form').validate({
+        rules: {
+            nombreapellido : "required",
+            email : {
+                required: true,
+                email: true
+            }
+        },
+        messages: {
+            nombreapellido : "Tu nombre y apellido son obligatorios. Obvio.",
+            email : "Un correo valido, por favor. No deberíamos tener que decir por favor."
+        },
+        highlight: function(element) {
+        $(element).addClass('is-danger');
+        },
+        unhighlight: function(element) {
+            $(element).removeClass('is-danger');
+        },
+        submitHandler : function(form) {
+            form.submit();
+        }
+    });
+});
